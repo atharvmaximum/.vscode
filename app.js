3b8838256c311b6c9545e7972dbc048e1f5b18bc
atharvmaximum <161740717+atharvmaximum@users.noreply.github.com>
2024-07-25 21:52:41 +0530

    operation2 js

---
+++ app.js
@@ -0,0 +1,119 @@
+//Introduction to Backend-
+//what Exactly node is ?
+//Amazon -
+//Sign in - username and password
+//Backend - 
+//request -response cycle - we are trying to communicate with backend to get the information of about these page it may helps by an DNS system . for eg - amazon etc. DNS is a combination of key and value(IP).
+//Node - > It is an javascript runtime environment.
+// Browser -> JS  is an engine which supports  diffterent JS engines are ->
+//V8 - chrome , spider-Monkey -> Mozilla , Chakra - Edge
+// Basically  Node is able to compile and interpreted of js code respectively.they are 99% simillar fact but there  is an 1% diffterence in it .
+// console.log("this is rupam sabale");
+
+// let a = 24;
+
+// console.log(`the age of rupam Sabale is ${a}`);
+//CLient - Server Model - as the client request the signal the server fulfill the client request so hence client - browser as well as server -  server respectively.
+//eg - Amazon.in etc. 
+//DNS - Domain Server System -  127.0.0.2 - It is an IPV4 address respectively for eg - airbnb .com etc. address of the server .
+//  It is an IPV6 address  for e.g - google and Facebook etc.
+//popular languages to build the backend is -
+// JS (Node) - Bun,Deno.
+// PHP
+//Java -SpringBoot
+//Scala
+//rust
+//ruby- Ruby on Rails
+//Go Lang
+//Python -Django,flask
+//purpose of application server - it belongs from the  client in which it understands the request of the server  and it gives us the response to the client whatever it required .
+// process of client- server communication : 
+//Database(collects past orders of data) -> Application server which receives past Orders of database -> so hence it gives a response on past orders and it sends to an client whatever it requires .eg amazon etc.
+//client-side rendering and server-side rendering concepts .
+//client-server is a two way-process - 
+// 1 accept the data from server 2 it sends  the data from the server respectively.
+//LTS -Long Term Support - every product has versioning in it.
+//V8 Engine -> Browser -> Machine -> Node 
+//set Timeout 
+//set Interval 
+//console log 
+// fetch 
+ // browser ->
+ //Node -> we do not have the browser 
+ //Node -> V8 +libuv + node related Packages + external Modules 
+
+//.createElement 
+//.getElementById
+
+// fetch();//Introduction to Backend-
+ //what Exactly node is ?
+ //Amazon -
+ //Sign in - username and password
+ //Backend - 
+ //request -response cycle - we are trying to communicate with backend to get the information of about these page it may helps by an DNS system . for eg - amazon etc. DNS is a combination of key and value(IP).
+ //Node - > It is an javascript runtime environment.
+ // Browser -> JS  is an engine which supports  diffterent JS engines are ->
+ //V8 - chrome , spider-Monkey -> Mozilla , Chakra - Edge
+ // Basically  Node is able to compile and interpreted of js code respectively.they are 99% simillar fact but there  is an 1% diffterence in it .
+ // console.log("this is rupam sabale");
+ 
+ // let a = 24;
+ 
+ // console.log(`the age of rupam Sabale is ${a}`);
+ //CLient - Server Model - as the client request the signal the server fulfill the client request so hence client - browser as well as server -  server respectively.
+ //eg - Amazon.in etc. 
+ //DNS - Domain Server System -  127.0.0.2 - It is an IPV4 address respectively for eg - airbnb .com etc. address of the server .
+ //  It is an IPV6 address  for e.g - google and Facebook etc.
+ //popular languages to build the backend is -
+ // JS (Node) - Bun,Deno.
+ // PHP
+ //Java -SpringBoot
+ //Scala
+ //rust
+ //ruby- Ruby on Rails
+ //Go Lang
+ //Python -Django,flask
+ //purpose of application server - it belongs from the  client in which it understands the request of the server  and it gives us the response to the client whatever it required .
+ // process of client- server communication : 
+ //Database(collects past orders of data) -> Application server which receives past Orders of database -> so hence it gives a response on past orders and it sends to an client whatever it requires .eg amazon etc.
+ //client-side rendering and server-side rendering concepts .
+ //client-server is a two way-process - 
+ // 1 accept the data from server 2 it sends  the data from the server respectively.
+ //LTS -Long Term Support - every product has versioning in it.
+ //V8 Engine -> Browser -> Machine -> Node 
+ //set Timeout 
+ //set Interval 
+ //console log 
+ // fetch 
+  // browser ->
+  //Node -> we do not have the browser 
+  //Node -> V8 +libuv + node related Packages + external Modules 
+  console.log("this is rupam sabale");
+ 
+ let a = 24;
+ const port = 3000;
+
+ http = require('http')
+
+
+ console.log(`the age of rupam Sabale is ${a}`);
+ setTimeout(() =>{
+     console.log("executed Later");
+ 
+ },1000)
+ /*node
+ .createElement 
+ .getElementById
+ */
+// fetch();
+  //node -  Internal Architecture of the node 
+  //Event -Driven Model - event queue , microsoft queue ,data queue,call STack ,
+  //client - server - *** -> root of the we development
+  // node - is the 70% of javascript ->
+  //related to  30% backend related Knowledge 
+  //always and always be writing js ----
+  //node Revision -
+  // BE - 
+  //what is map in js ?
+  //call back 
+
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+
+
+// console.log("hello world");
+//2 diffterent types of machines communication has been established 
+//-> Destructuring - 70%of JS
+// const arr1 = ['naman',18];
+// const[name,age] = arr1;
+// console.log(name,age);// output naman ,18
+//array based destructuring works on the principle  of sequencing,
+//object based destructuring works on the principle of key matching .
+//Modules in js -
+//seperation of concerns 
+//HTML -
+//why opt for a diffternt JS file?
+
+//<script>
+//</script>
+//BACKEND ->
+//data flow  journey ->
+//file structuring -> how will be  organising the BE code so that we  are able to acheive the seperation of concerns effectively.
+//three diffterent js files ->
+//one.js
+//two.js
+//three.js
+//File- System -> File Moduling ->
+//1]Common JS module system(default)
+//2]ES6 Module System
+//syntatic diffternce 
+
+//Main file - two.js
+//add and Subtract 
+// const operations =  require("./operation.js");//importing a particular file data.
+// console.log(operations);
+
+// const sum = operations.add(a,b);
+// console.log(sum);
+// import add from './operation.js'
 const {add} = require('./operation.js')
 const sum = add(1,2);
 console.log(sum);
